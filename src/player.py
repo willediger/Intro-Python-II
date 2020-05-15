06d374abb49ebbc3039839100d15deac0119faa2
Will Ediger <will.ediger@gmail.com>
2020-05-14 21:31:54 -0500

    added items to Player and Room classes

--- src/player.py
+++ src/player.py
@@ -2,9 +2,26 @@
 # currently.
 
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, items = []):
         self.name = name
         self.current_room = current_room
+        self.items = items
     def __str__(self):
         return f"""Name: {self.name}
 Current Room: {self.current_room.name}"""
+
+    def pick_up_item(self, room, item):
+        self.items.append(item)
+        room.remove_item(item)
+        return self.items
+    
+    def drop_item(self, room, item):
+        self.items.remove(item)
+        room.add_item(item)
+        return self.items
+
+    def get_items(self):
+        if self.items == []:
+            print("No items in bag")
+        for item in self.items:
+            print(item)
