48c8b02e9c0ecf67d4bc9ae5a1eae58d80a705d9
Will Ediger <will.ediger@gmail.com>
2020-05-14 21:32:45 -0500

    added items to rooms

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,41 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", [
+                         Item('sword', 'a steel sword'),
+                         Item('axe', 'a steel axe')
+                     ]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", [
+                         Item('helmet', 'a steel helmet'),
+                         Item('gloves', 'leather gloves')
+                     ]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", [
+                         Item('ring', 'a silver ring'),
+                         Item('shield', 'a steel shield')
+                     ]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", [
+                         Item('necklace', 'a gold necklace'),
+                         Item('bracelet', 'a gold bracelet')
+                     ]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", [
+                         Item('chest', 'a mysterious treasure chest'),
+                         Item('onion', 'an onion')
+                     ]),
 }
 
 
